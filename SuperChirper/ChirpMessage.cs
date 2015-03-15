0f70194cac9f3c32fa5a02675c5517444120a98a
AtheMathmo <lucasj94@hotmail.co.uk>
2015-03-15 09:24:29 +0000

    Initial Commit

---
+++ SuperChirper/ChirpMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ColossalFramework;
+using ColossalFramework.UI;
+using ICities;
+using UnityEngine;
+
+
+public class ChirpMessage : IChirperMessage
+{
+    private string message;
+    private string sender;
+    private uint id;
+
+    public ChirpMessage(string sender, string message, uint id)
+    {
+        this.sender = sender;
+        this.message = message;
+        this.id = id;
+    }
+
+    public uint senderID
+    {
+        get { return id; }
+    }
+
+    public string senderName
+    {
+        get { return sender; }
+    }
+
+    public string text
+    {
+        get { return message; }
+    }
+}
---
+++ SuperChirper/SuperChirperMod.cs
@@ -0,0 +1,47 @@
+﻿using ColossalFramework.UI;
+using ICities;
+using System;
+using UnityEngine;
+
+namespace SuperChirper
+{
+    public class SuperChirperMod : IUserMod
+    {
+        private static UIButton clearButtonInstance;
+        private static UIButton muteButtonInstance;
+
+        public static UIButton ClearButtonInstance
+        {
+            get
+            {
+                return clearButtonInstance;
+            }
+            set
+            {
+                clearButtonInstance = value;
+            }
+        }
+
+        public static UIButton MuteButtonInstance
+        {
+            get
+            {
+                return muteButtonInstance;
+            }
+            set
+            {
+                muteButtonInstance = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return "SuperChirper"; }
+        }
+
+        public string Description
+        {
+            get { return "Makes Chirpy beautiful."; }
+        }
+    }    
+}
