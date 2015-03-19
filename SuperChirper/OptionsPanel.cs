330b16a5616510d98c3a3cc31a17a54b356cfc7e
AtheMathmo <lucasj94@hotmail.co.uk>
2015-03-19 18:31:10 +0000

    Added class for options panel

---
+++ SuperChirper/OptionsPanel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ColossalFramework.Plugins;
+using ColossalFramework.UI;
+using ICities;
+using UnityEngine;
+
+namespace SuperChirper
+{
+    class OptionsPanel : UIPanel
+    {
+        
+        public override void Start()
+        {
+            this.AddUIComponent(typeof(UIDragHandle));
+
+            this.backgroundSprite = "GenericPanelLight";
+            this.color = new Color32(120, 120, 255, 200);
+            this.width = 300;
+            this.height = 200;
+
+            // Default to hidden
+            this.Hide();
+        }
+
+        protected override void OnResolutionChanged(Vector2 previousResolution, Vector2 currentResolution)
+        {
+            base.OnResolutionChanged(previousResolution, currentResolution);
+        }
+    }
+}
